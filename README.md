cff190e905381c28045fd4fb9862b27aa5e98396
Leonard Koll <leko@netlight.com>
2020-04-09 12:47:57 +0200

    done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# coffeerandomizer
+# Coffee Randomizer
+
+Professional, extensively tested and complex randomizer to get people together for a coffee. We've involved multiple teams for requirements engeneering, arcitecture, user experience improvement and testing.
+
+This tool will get an extensive documentation someday, somewhere. Until then, please figure out by yourself how to use it.
---
+++ randomize.py
@@ -0,0 +1,33 @@
+import random
+
+print("Who is involved? List all people in a txt file, add a linebreak after each person.")
+path = input("Path to your txt: ")
+print()
+
+f = open(path, 'r')
+with open(path) as f:
+    people = f.read().splitlines()
+f.close()
+
+size = int(input("What's your preferred group size? "))
+
+random.shuffle(people)
+groups = []
+current = 0
+while len(people) > 0:
+    current = len(groups)
+    groups += [[]]
+    while len(people) > 0 and len(groups[current])<size:
+        groups[current] += [people.pop(0)]
+    if len(people) < size:
+        while len(people) > 0:
+            current = 0
+            groups[current] += [people.pop(0)]
+            current += 1
+            if current >= len(groups):
+                current = 0
+
+print()
+print("How about this?")
+for group in groups:
+    print(" - " + ", ".join(group))
